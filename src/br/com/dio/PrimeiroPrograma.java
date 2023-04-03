93a73775ecb4e767dfc10453a209a4f76ab8bea8
fabioreiss <fabio83bass@gmail.com>
2023-04-03 20:21:56 -0300

    First commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,7 +5,6 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
        /* int a = 5;
         int b = 3;
